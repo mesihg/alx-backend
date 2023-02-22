835c2e618f280bb580e1d08a55a14c29ffae0987
Mesih Grmay <mesihgrmay@yahoo.com>
2023-02-22 13:26:32 +0000

    Task 3

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" LRU caching module """
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ Represents an object that allows storing and
+        retrieving items from a dictionary
+    """
+
+    def __init__(self):
+        """ Initialize the cache """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ Add item to the cache """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                l_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", l_key)
+            self.cache_data[key] = item
+        else:
+            self.cache_data.move_to_end(key, last=False)
+
+    def get(self, key):
+        """ Retrieve an item from cache """
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
