decee2e75e0284520a628f3ca858e1906f3d4376
Mesih Grmay <mesihgrmay@yahoo.com>
2023-03-30 08:44:42 +0000

    Task 4

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -2,34 +2,38 @@ import redis from 'redis';
 
 const client = redis.createClient();
 
-client.on('connect', () => {
-  console.log('Redis client connected to the server');
-});
-
-client.on('error', (error) => {
-  console.log(`Redis client not connected to the server: ${error}`);
-});
-
-function setNewSchool(schoolName, value) {
-  client.set(schoolName, value, redis.print);
+function createHash (hashName, fieldName, fieldValue) {
+  client.hmset(hashName, fieldName, fieldValue, redis.print);
 }
 
-async function displaySchoolValue(schoolName) {
-  const getAsync = promisify(client.get).bind(client);
-  const value = await getAsync(schoolName);
-  console.log(value);
-}
-
-function createHash(hashName, values) {
-  client.hmset(hashName, values, redis.print);
-}
-
-function displayHash(hashName) {
+function displayHash (hashName) {
   client.hgetall(hashName, (error, value) => {
     if (error) console.log(error);
     console.log(value);
   });
 }
 
-createHash('HolbertonSchools', 'Portland', '50', 'Seattle', '80', 'New York', '20', 'Bogota', '20', 'Cali', '40', 'Paris', '2');
-displayHash('HolbertonSchools');
+function getAll () {
+  const obj = {
+    Portland: 50,
+    Seattle: 80,
+    'New York': 20,
+    Bogota: 20,
+    Cali: 40,
+    Paris: 2
+  };
+
+  for (const [field, value] of Object.entries(obj)) {
+    createHash('HolbertonSchools', field, value);
+  }
+  displayHash('HolbertonSchools');
+}
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+  getAll();
+});
+
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error}`);
+});
